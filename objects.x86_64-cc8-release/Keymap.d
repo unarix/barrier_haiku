d682b13627280766f8b4d3ea3df05e7978966fe3
nhtello <team@unarix.com.ar>
2020-11-21 15:25:06 -0300

    Build

---
+++ objects.x86_64-cc8-release/Keymap.d
@@ -0,0 +1,4 @@
+# --- DO NOT MODIFY THIS LINE -- AUTO-DEPENDS FOLLOW ---
+objects.x86_64-cc8-release/Keymap.o : Keymap.h
+
+# --- DO NOT MODIFY THIS LINE -- AUTO-DEPENDS PRECEDE ---
---
+++ objects.x86_64-cc8-release/haiku-ubarrier.d
@@ -0,0 +1,5 @@
+# --- DO NOT MODIFY THIS LINE -- AUTO-DEPENDS FOLLOW ---
+objects.x86_64-cc8-release/haiku-ubarrier.o : haiku-ubarrier.h ATKeymap.h \
+    uBarrier.h Keymap.h
+
+# --- DO NOT MODIFY THIS LINE -- AUTO-DEPENDS PRECEDE ---
